960423c739f782ecccb996d4a98a85d97aef37b2
kristintv <kristin161098@mail.ru>
2017-09-28 08:45:12 +0700

    Fixed

--- src/main.c
+++ src/main.c
@@ -5,26 +5,29 @@
 int main() 
 {
 	int a,b,c,D,i;
-	float x;
+	float x, x1, x2;
 	printf("a*(x*x)+b*x+c=0\n");
-	scanf("%d%d%d",&a, &b, %c);
-
+	printf("Input a=");
+	scanf("%d",&a);
+	printf("Input b=");
+	scanf("%d",&b);
+	printf("Input c=");
+	scanf("%d",&c);
 	D = Discr(a, b, c);
 	i = Check(D);
 
 	switch (i) {
 		case 0:
 			x = ((-1) * b) / (2 * a);
-			printf ("%f", x);
+			printf ("%.2f", x);
 			break;
 		case 1:
 			printf ("Has no roots");
 			break;
 		case 2:
-			x1 = ((-1) * b + sqrt(D)) / (2 * a);
-			printf("%f",x);			
-			x = ((-1) * b - sqrt(D)) / (2 * a);
-			printf("%f",x);
+			x1 = ((-1) * b + sqrt(D)) / (2 * a);			
+			x2 = ((-1) * b - sqrt(D)) / (2 * a);
+			printf("First roots:%.2f.\nSecond roots:%.2f.\n",x1, x2);
 			break;
 	}
 	return 0;
