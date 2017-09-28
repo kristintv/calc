ee18ebcd4f4690bd1663ebf19964800e7ace308d
kristintv <kristin161098@mail.ru>
2017-09-28 17:39:03 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int main()
 	scanf("%d", &b);
 	printf("Input c = ");
 	scanf("%d", &c);
-	if ((a == 0) || ((a == 0) && (b == 0)) || ((a == 0) && (b ==0) && (c == 0)){
+	if ((a == 0) || ((a == 0) && (b == 0)) || ((a == 0) && (b ==0) && (c == 0))){
 		printf("INVALID_ARGUMENTS!!!");
 		break;
 	}
